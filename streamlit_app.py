dd4eab2c55a099e0d29d64814a95c5686cae3078
acspedro2 <acspedro2@gmail.com>
2025-08-27 07:35:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,9 +131,9 @@ def preencher_pdf_formulario(paciente_dados):
         sexo = str(paciente_dados.get("Sexo", "")).strip().upper()
         can.setFont("Helvetica-Bold", 12)
         if sexo.startswith('F'):
-            can.drawString(11.8 * cm, 22.9 * cm, "X") # Ajuste final
+            can.drawString(12.2 * cm, 22.9 * cm, "X") # Ajuste final
         elif sexo.startswith('M'):
-            can.drawString(12.7 * cm, 22.9 * cm, "X") # Ajuste final
+            can.drawString(13.1 * cm, 22.9 * cm, "X") # Ajuste final
         
         can.save()
         packet.seek(0)
